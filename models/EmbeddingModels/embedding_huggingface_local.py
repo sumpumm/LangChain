97ed96e3fa68c945546fa3a6a59810e767a11f54
sumpumm <samarpan69.piya@gmail.com>
2026-04-08 10:08:41 +0545

    embeddings created

---
+++ models/EmbeddingModels/embedding_huggingface_local.py
@@ -0,0 +1,21 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+
+embedding = HuggingFaceEmbeddings(model_name = "sentence-transformers/all-MiniLM-L6-v2")
+
+text = "My name is samarpan"
+
+#embedding a single sentence
+vector = embedding.embed_query(text)
+
+print(vector)
+
+#embediing documents
+documents=[
+    "My name is samarpan",
+    "i am 22 years old",
+    "i live in jhamsikhel"
+]
+
+vector2= embedding.embed_documents(documents)
+
+print(str(vector2))
